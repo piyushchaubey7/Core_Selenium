0fee263fc2a1c99b42da8b8d2d1c63c9c36f41ab
Piyush Chaubey <piyush90261814@gmail.com>
2026-06-28 03:10:38 +0530

    pop ,Alert update code

--- Core_Selenium/src/test/java/Web_Elements/Modals_and_Popups.java
+++ Core_Selenium/src/test/java/Web_Elements/Modals_and_Popups.java
@@ -3,6 +3,7 @@ package Web_Elements;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.TargetLocator;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Modals_and_Popups {
@@ -14,30 +15,50 @@ public class Modals_and_Popups {
 		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
 		
 		driver.manage().window().maximize();
-		
-		driver.findElement(By.xpath("//button[normalize-space()='Click for JS Alert']")).click();
-		
-		
-    	Alert alert = driver.switchTo().alert();
-    	
-    	Thread.sleep(2000);
-		
-
-		// Alert Text
-		System.out.println(alert.getText());
-
-		// Accept
-		alert.accept();
-
-		/*// Dismiss
-		alert.dismiss();
-
-		// Prompt
-		alert.sendKeys("Piyush");
-		alert.accept();
-		*/
+		// JS Alert
+		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
 		
 		Thread.sleep(2000);
+
+		Alert a = driver.switchTo().alert();
+
+		System.out.println(a.getText());
+
+		a.accept();
+
+
+		// JS Confirm - OK
+		driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
+
+		a = driver.switchTo().alert();
+
+		System.out.println(a.getText());
+
+		a.accept();
+
+
+		// JS Confirm - Cancel
+		driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
+
+		a = driver.switchTo().alert();
+
+		System.out.println(a.getText());
+
+		a.dismiss();
+
+
+		// JS Prompt
+		driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
+
+		a = driver.switchTo().alert();
+
+		System.out.println(a.getText());
+
+		a.sendKeys("Piyush");
+
+		a.accept();
+		
+		Thread.sleep(3000);
 	}
 
 }
